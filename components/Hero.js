4596676c474e1c7e99de883c4364e454cc938937
Colby Cooke <colby.n.cooke@vanderbilt.edu>
2020-06-26 10:27:03 -0400

    added linear gradient to hero

--- components/Hero.js
+++ components/Hero.js
@@ -1,27 +1,43 @@
-import * as React from 'react';
-import { Image, View, StyleSheet } from 'react-native';
-
+import * as React from "react";
+import { ImageBackground, View, StyleSheet, Text } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 class HeroComponent extends React.Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Image source={require('../img/work.jpg')} style={styles.heroHeaderStyle}/>
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+        <ImageBackground
+          source={require("../img/croppedworker.jpg")}
+          style={styles.heroHeaderStyle}
+          resizeMode="contain"
+        >
+          <LinearGradient
+            colors={["rgba(98,29,235,0.3)", "rgba(98,29,235,0.3)"]}
+            style={{
+              position: "absolute",
+              left: 0,
+              right: 0,
+              top: 6,
+              height: 188,
+            }}
+          >
+          </LinearGradient>
+        </ImageBackground>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    heroHeaderStyle: {
-        height: 200,
-        width: '100%'
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  heroHeaderStyle: {
+    height: 200,
+    width: "100%",
+  },
 });
 
 export default HeroComponent;
